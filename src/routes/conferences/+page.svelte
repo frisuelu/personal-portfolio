e169a9c8205038465f99a13f196e1d734d7779ac
frisuelu <manuelsoju@hotmail.com>
2023-09-25 07:02:13 +0200

    add template for conferences

--- src/routes/conferences/+page.svelte
+++ src/routes/conferences/+page.svelte
@@ -1,1 +1,33 @@
-<h1>Conferences page</h1>
+<section class="pt-20 lg:pt-[120px] pb-10 lg:pb-20">
+  <div class="container">
+    <div class="flex flex-wrap justify-center -mx-4">
+      <div class="w-full px-4">
+        <div class="text-center mx-auto mb-[60px] lg:mb-20 max-w-[510px]">
+          <h2
+            class="
+                  font-bold
+                  text-3xl
+                  sm:text-4xl
+                  md:text-[40px]
+                  text-dark
+                  mb-4
+                  "
+          >
+            <b>Talks, conferences and content</b>
+          </h2>
+          <p class="text-base text-body-color">
+            I have participated in multiple university talks and conferences
+            where I have had the opportunity to share my knowledge and
+            experiences around data with both technical and young people
+            interested in getting into the field of Data Science, as well as
+            non-technical people that are trying to understand what data entails
+            and how it will impact them (this goes doublefold for businesses
+            trying to become data-driven).
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- Add below the embedded content from Youtube, Twitch, LinkedIn... -->
