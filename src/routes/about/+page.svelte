9ac6cbee7db9b5f64162b7a4387971df303b98dc
frisuelu <manuelsoju@hotmail.com>
2023-09-30 12:58:40 +0200

    add content to "about" page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,10 +1,7 @@
 <svelte:head>
   <title>About me - Manuel Souto Juan</title>
   <meta charset="UTF-8" />
-  <meta
-    name="description"
-    content="More details about Manuel Souto Juan"
-  />
+  <meta name="description" content="More details about Manuel Souto Juan" />
   <meta
     name="keywords"
     content="Data Science, AI, Machine Learning, Technology"
@@ -18,7 +15,124 @@
   <h1>
     <b>About me</b>
   </h1>
+</div>
+<div class="prose">
   <p>
-    <br />WIP
+    <br />Hi! My name is Manuel, and I’m a Data Scientist based in Spain. I was
+    born on November 8th, 1996 in northern Spain (in a little green place called
+    Asturias) where I currently live at.
+  </p>
+  <p>
+    As all kids do, I was fascinated from a young age about everything; the
+    nagging kid that doesn't stop asking "but why?". Since I was mostly
+    interested in science, the universe and similar topics, my parents decided
+    to just buy me lots of scientific magazines for kids so I would shut up
+    (spoiler: I didn't, I just asked even more stuff 😅).
+  </p>
+  <p>
+    No wonder I ended where I'm at with all of those things in my mind. I hold
+    degrees in both Physics and Mathematics from the University of Oviedo.
+    During my tenure, I worked on the analysis of data from the (quite recent at
+    the time) LHC Run 2 at CERN in the search for dark matter candidates, where
+    a mix of Physics, C++/Python and Statistics showed how these disciplines
+    combine to provide great insights. On the maths side, I applied <i
+      >fuzzy logic</i
+    > to the statistical analysis of qualitative "unmeasurable" variables. Check
+    out the papers here:
+  </p>
+  <ul>
+    <li>
+      <a
+        href="src/lib/theses/TFGManuelSouto-2018_2019-Physics.pdf"
+        download="TFGManuelSouto-2018_2019-Physics.pdf"
+        >Dark matter search associated with top quarks in the CMS experiment at
+        LHC (CERN). <i>University of Oviedo, 2018-2019</i></a
+      >
+    </li>
+    <li>
+      <a
+        href="src/lib/theses/TFGManuelSouto-2018_2019-Maths.pdf"
+        download="TFGManuelSouto-2018_2019-Maths.pdf"
+        >Fuzzy characterization of the distribution of a random variable.
+        Applications. <i>University of Oviedo, 2018-2019</i></a
+      >
+    </li>
+  </ul>
+  <p>
+    I loved both particle physics and statistics; however, I quickly realized
+    the life of a researcher was not for me, and I wanted to try my part in the
+    private sector. Thus, I embarked on getting a Master’s Degree in Advanced
+    Statistical Analysis in the University of Salamanca, where I started my
+    first steps into the Big Data and Machine Learning world. Check it here:
+  </p>
+  <ul>
+    <li>
+      <a
+        href="src/lib/theses/TFMManuelSouto-2019_2020-Stats.pdf"
+        download="TFMManuelSouto-2019_2020-Stats.pdf"
+        >Text mining with open-source software: DtmVic & IRaMuTeQ. Comparative
+        study applied to climate change.
+        <i>University of Salamanca, 2019-2020</i></a
+      >
+    </li>
+  </ul>
+  <p>
+    Afterwards, I got my first position at <a
+      href="https://www.bedrockdbd.com"
+      title="Bedrock - Humanised Intelligence">Bedrock</a
+    >, a small Data Science consulting firm in Asturias, where I have been
+    working for almost three years now.
+  </p>
+  <h2>And I also have a life outside of work 😉</h2>
+  <p>
+    I’m a curious person by nature, always trying to learn new things. This
+    includes both on the ML/AI space, where each week it seems that the advances
+    are leaps and bounds above what used to happen in the era pre-ChatGPT
+    (honestly, I wish there was more mainstream material on other tasks such as
+    CV or recommendation systems). Platforms such as <a
+      href="https://alphasignal.ai/">Alpha Signal</a
+    >
+    are amazing to keep up to date with all the new things that are coming out daily!
+  </p>
+  <p>
+    I also love programming, where I mostly use Python in my day-to-day job, but
+    I have also used C++, JS, Go and Julia for smaller projects. I have also
+    delved into embedded devices, IoT and microcontrollers; for example,
+    interconnecting several devices using the MQTT protocol to deliver real-time
+    sensor outputs to a physical LED dashboard for further analysis. Check all
+    of these and more in the <a href="/projects">projects</a> folder if you haven't
+    yet!
+  </p>
+  <p>
+    As a matter of fact, this website you're reading went through many
+    revisions! When I started it I wanted to develop my Go skills and I built a
+    working back-end with routing, however getting the front-end ready and <i
+      >pretty</i
+    >
+    was kind of a pain. In the end I settled with Svelte since I love the slick designs,
+    reactivity, and overall support of the community!
+  </p>
+  <p>
+    I'm also a FOSS advocate, using Linux and open-source tools as much as I
+    can. I have been daily driving Linux on my desktop for over a year now, and
+    honestly it's been amazing coming from the <i>great</i> experience Windows is.
+    Although I have hacked my way around the system and know a lot of it, right now
+    I'm trying to deepen my knowledge.
+  </p>
+  <p>
+    And yes, I have a laptop with Arch (btw). And yes, I am using NeoVim to
+    develop my projects. Deal with it.
+  </p>
+  <p>
+    <small>
+      But if you learn how to exit it, please let me know, I'm stuck in
+      limbo...🥲</small
+    >
+  </p>
+  <p>
+    As a side note, I also love tennis, music (who doesn't?), sci-fi books
+    (Brandon Sanderson is a genius), videogames (<a
+      href="https://nobaraproject.org/">Linux PC</a
+    > all the way!) and travelling when I have free time!
   </p>
 </div>
--- src/routes/skillset/+page.svelte
+++ src/routes/skillset/+page.svelte
@@ -16,9 +16,57 @@
 
 <div align="center" class="landing_title">
   <h1>
-    <b>Skills and tools used on my job</b>
+    <b>Skillset and tools I am proficient in</b>
   </h1>
   <p>
-    <br />WIP
+    <br />Strong statistical base: hypothesis tests, ANOVA, inference,
+    visualization, Monte Carlo simulations, bootstraping. • Multivariate
+    statistical analyses and dimensionality reduction techniques: clustering,
+    supervised and unsupervised classification, PCA, correlation analysis or
+    STATIS methods, among others. • ML and DL algorithms: neural networks,
+    association rules, random forest, Naive Bayes, SVM, Transformers, CNNs or
+    Reinforcement Learning. • Extensive experience working with relational
+    databases, especially focused on RDBMS such as PostgreSQL, MySQL, Google
+    BigQuery and PowerQuery. • Advanced knowledge of general programming and OOP
+    with Python. • Knowledge of the main libraries employed in the Data Science
+    field: Numpy, Pandas, Matplotlib, Keras, TensorFlow, PyTorch, Transformers.
+    • Advanced knowledge of the Office stack. • Experience working with software
+    and platforms such as Docker or Databricks. Technology stack experience ◦
+    Python ◦ C++ ◦ AWS ◦ Google Analytics ◦ PostgreSQL ◦ Metabase ◦ Docker ◦
+    Databricks ◦ PowerQuery ◦ Matlab/Octave ◦ Bash ◦ GCP ◦ GSC ◦ MySQL ◦ Apache
+    Superset ◦ Flask/Django ◦ Golang ◦ Javascript ◦ R ◦ SPSS ◦ Azure ◦ Hubspot ◦
+    PowerBI ◦ Git ◦ Arduino ◦ TinyGo
+  </p>
+  <p>
+    As part of a smaller team, I have not just learned technical aspects of my
+    position but many transversal aspects of how to run a company as well as the
+    much required <i>soft skills</i>. These include good communication skills,
+    working together with clients to bridge the gap between their business needs
+    and the abilities that a Data Scientist offers, proficiency in spoken and
+    written English aside through client interactions in the international space.
+  </p>
+  <p>
+    One of the most important learnings of my professional career has been my
+    experience at a startup such as Bedrock, since a small team also means you
+    get to understand a lot of other core business areas that Data Scientists
+    usually struggle to understand (finance, new business/commercial meetings,
+    direct client relationships...). I have learned that solutions, either in
+    data or other disciplines, must first be based on business information to
+    accurately solve the problems at hand. The arbitrary use of technology is
+    ill-advised, and an informed roadmap usually yields better results for a
+    fraction of the effort, while purely technical developments that are not
+    based on business needs will fall on deaf ears.
+  </p>
+  <p>
+    I have also been in direct charge of client accounts, leading the technical
+    process of understanding the needs of the client, designing the business
+    plan, and developing the solutions together with my colleagues. Another
+    important part of my experience has been researching, planning and attending
+    new business meetings constantly, as in a small team all members must
+    support both lead generation and conversion into projects. Through the use
+    of the Service Design and Agile methodologies, I have grown professionally
+    and I have gained a holistic view of a full project design and development,
+    so I believe I have a broad view regarding the design and communication
+    around technical projects.
   </p>
 </div>
