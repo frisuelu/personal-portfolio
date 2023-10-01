3a8dbe8f3065bb23e04d497fe020dd6cb9233f09
frisuelu <manuelsoju@hotmail.com>
2023-10-01 20:06:54 +0200

    remove config files

--- .svelte-kit/generated/client/nodes/5.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/conferences/+page.js";
-export { universal };
-export { default as component } from "../../../../src/routes/conferences/+page.svelte";
