4eae1a584ba76ddbd41ef6610c1ef5bd3ec5d76f
Frisuelu <manuelsoju@hotmail.com>
2024-09-24 19:19:35 +0200

    Updates

---
+++ _app/immutable/chunks/paths.aebec4a4.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_4qwa71)==null?void 0:s.base)??"/personal-portfolio";var a;const t=((a=globalThis.__sveltekit_4qwa71)==null?void 0:a.assets)??e;export{t as a,e as b};
