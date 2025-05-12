9e6c884f1798b29beb33056a54a9114cd9997ea2
Frisuelu <manuelsoju@hotmail.com>
2025-05-12 18:54:22 +0200

    Updates

---
+++ _app/immutable/chunks/paths.5bc6790f.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1uerr2y)==null?void 0:s.base)??"/personal-portfolio";var e;const t=((e=globalThis.__sveltekit_1uerr2y)==null?void 0:e.assets)??a;export{t as a,a as b};
